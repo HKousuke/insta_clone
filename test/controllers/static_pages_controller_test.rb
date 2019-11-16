680594210972c93a7c8d2877eff008936161bcaa
EC2 Default User <ec2-user@ip-172-31-15-133.us-east-2.compute.internal>
2019-11-16 04:21:30 +0000

    user_create

---
+++ test/controllers/static_pages_controller_test.rb
@@ -0,0 +1,10 @@
+require 'test_helper'
+
+class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  
+  test "should get root" do
+    get root_path
+    assert_response :success
+    assert_select "title", "Insta_clone_app"
+  end
+end
