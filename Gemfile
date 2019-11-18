71484e90616adb5a7034430d0b6c3d2f15aca4a4
EC2 Default User <ec2-user@ip-172-31-15-133.us-east-2.compute.internal>
2019-11-18 13:37:54 +0000

    account-activation

--- Gemfile
+++ Gemfile
@@ -6,6 +6,8 @@ git_source(:github) do |repo_name|
 end
 
 gem 'rails',        '5.1.6'
+gem 'faker',          '1.7.3'
+gem 'will_paginate', '3.1.5'
 gem 'bcrypt',         '3.1.12'
 gem 'bootstrap-sass', '3.3.7'
 gem 'puma',         '3.9.1'
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,7 +27,10 @@ Rails.application.configure do
   end
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :test
+  host = 'https://3252c85a78a5417d952d5b56b6cd7eab.vfs.cloud9.us-east-2.amazonaws.com/' #　HEROKU環境のURL
+  config.action_mailer.default_url_options = { host: host, protocol: 'https' }
 
   config.action_mailer.perform_caching = false
 
